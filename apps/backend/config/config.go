78a16f87ccb1e3042769c8dcaa06a5bc1f46500e
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-05-25 12:06:08 +0530

    fix: fixed the config db file for docker

--- apps/backend/config/config.go
+++ apps/backend/config/config.go
@@ -22,14 +22,19 @@ func LoadEnv(){
 
 
 func ConnectDB() *gorm.DB {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable" ,
-			os.Getenv("DB_HOST"),
-			os.Getenv("DB_USER"),
-			os.Getenv("DB_PASSWORD"),
-			os.Getenv("DB_NAME"),
-			os.Getenv("DB_PORT"),
-	)
 
+    dsn := fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+        os.Getenv("DB_HOST"),
+        os.Getenv("DB_USER"),
+        os.Getenv("DB_PASSWORD"),
+        os.Getenv("DB_NAME"),
+        os.Getenv("DB_PORT"),
+        os.Getenv("DB_SSL"),
+    )
+
+
+	fmt.Printf(dsn)
 	db, err := gorm.Open(postgres.Open(dsn) , &gorm.Config{})
 
 	if err != nil {
