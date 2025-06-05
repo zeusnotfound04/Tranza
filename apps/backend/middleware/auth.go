1d2d0dbe2af0ed495b15b64c1fdfa6cdc024bea9
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-06-06 04:30:53 +0530

    feat: made the middeware

---
+++ apps/backend/middleware/auth.go
@@ -0,0 +1,62 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/zeusnotfound04/Tranza/utils" // Assuming utils package is in this path
+)
+
+var jwtSecret = []byte(utils.GetJWTSecret()) // Function to get secret from utils
+
+// JWTAuthMiddleware provides a JWT authentication middleware
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header required"})
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == authHeader { // No "Bearer " prefix
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Could not find bearer token in Authorization header"})
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			// Make sure that the token method conform to "SigningMethodHMAC"
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrSignatureInvalid
+			}
+			return jwtSecret, nil
+		})
+
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token signature"})
+				c.Abort()
+				return
+			}
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token", "details": err.Error()})
+			c.Abort()
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			// Set user claims in context
+			c.Set("userID", claims["user_id"])
+			c.Set("email", claims["email"])
+			c.Set("username", claims["username"])
+			c.Next()
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims"})
+			c.Abort()
+			return
+		}
+	}
+}
