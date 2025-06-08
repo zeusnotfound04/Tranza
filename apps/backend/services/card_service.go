873086506520eb499e84448c91c54748d38620c9
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-06-09 01:48:47 +0530

    fix: modified the card service

--- apps/backend/services/card_service.go
+++ apps/backend/services/card_service.go
@@ -16,19 +16,19 @@ func NewCardService(repo *repositories.CardRepository) *CardService {
 	return &CardService{Repo: repo}
 }
 
-func (s *CardService) LinkCard(ctx context.Context , card *models.LinkedCard) erro,r {
+func (s *CardService) LinkCard(ctx context.Context , card *models.LinkedCard) error {
 	// Add validations if needed (e.g., max limit check)
-	return s.Repo.Create(card)
+	return s.Repo.Create(ctx , card)
 }
 
 func (s *CardService) GetCards(ctx context.Context,  userID uint) ([]models.LinkedCard, error) {
-	return s.Repo.GetByUser(userID)
+	return s.Repo.GetByUser(ctx, userID)
 }
 
 func (s *CardService) DeleteCard(ctx context.Context, cardID, userID uint) error {
-	return s.Repo.Delete(cardID, userID)
+	return s.Repo.Delete(ctx, cardID, userID)
 }
 
-func (s *CardService) UpdateCardLimit(ctx context.Context, card *models.LinkedCard) error {
-	return s.Repo.UpdateLimit(ctx )
+func (s *CardService) UpdateCardLimit(ctx context.Context, cardID uint, userID uint , limit float64) error {
+	return s.Repo.UpdateLimit(ctx , cardID , userID , limit)
 }
