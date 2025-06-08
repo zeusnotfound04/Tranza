202a9540006755dd040e093cd10a8af6de8a9a35
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-06-09 01:45:55 +0530

    fix: modifed the routes

--- apps/backend/routes/routes.go
+++ apps/backend/routes/routes.go
@@ -3,32 +3,42 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/zeusnotfound04/Tranza/controllers"
-	"github.com/zeusnotfound04/Tranza/middleware" // This should map to the 'middlewares' package
+	middlewares "github.com/zeusnotfound04/Tranza/middleware"
 	"github.com/zeusnotfound04/Tranza/repositories"
 	"github.com/zeusnotfound04/Tranza/services"
 	"gorm.io/gorm"
 )
 
 func SetupRoutes(r *gin.Engine, db *gorm.DB) {
-	api := r.Group("/api")
+	api := r.Group("/api/v1")
+
 
 	api.POST("/register", controllers.SignupHandler)
 	api.POST("/login", controllers.LoginHandler)
-
-
-	authGroup := api.Group("")
-	authGroup.Use(middlewares.JWTAuthMiddleware())
-
 	api.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"message": "pong"})
 	})
 
-	cardRepo := repositories.NewCardRepository(db)
-	cardService := services.NewCardService(cardRepo)
-	cardController := controllers.NewCardController(cardService)
 
-	authGroup.POST("/cards", cardController.LinkCard)
-	authGroup.GET("/cards", cardController.GetCards)
-	authGroup.DELETE("/cards/:id", cardController.DeleteCard)
-	authGroup.PUT("/cards/:id/limit", cardController.UpdateLimit)
+	cardRepo := repositories.NewCardRepository(db)
+	txnRepo := repositories.NewTransactionRepository(db)
+	cardService := services.NewCardService(cardRepo)
+	paymentService := services.NewPaymentService(cardRepo, txnRepo)
+
+	
+	cardController := controllers.NewCardController(cardService)
+	transactionController := controllers.NewTransactionController(paymentService)
+
+
+	auth := api.Group("")
+	auth.Use(middlewares.JWTAuthMiddleware())
+
+	auth.POST("/cards", cardController.LinkCard)
+	auth.GET("/cards", cardController.GetCards)
+	auth.DELETE("/cards/:id", cardController.DeleteCard)
+	auth.PUT("/cards/:id/limit", cardController.UpdateLimit)
+
+	
+	auth.POST("/transactions", transactionController.MakePayment)
+	auth.GET("/transactions", transactionController.GetUserTransactions)
 }
